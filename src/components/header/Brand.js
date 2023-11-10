4cc35e95a8a3ea2117af9dde992508aa4095aec9
Martin Velazquez <mlvmatute@gmail.com>
2023-11-09 23:59:03 -0300

    pre-entrega1Velazquez

---
+++ src/components/header/Brand.js
@@ -0,0 +1,9 @@
+import brand from "../../img/brand.svg";
+
+const Brand = () => {
+    return(
+            <img src={brand} alt="Maxe" title="Maxe"></img>
+    )
+}
+
+export default Brand;
---
+++ src/components/header/NavBar.js
@@ -0,0 +1,24 @@
+import Brand from "./Brand";
+
+const NavBar = () => {
+
+    return (
+
+            <header>
+                <div className="containerBrand">
+                    <Brand/>
+                </div>
+
+                <nav className="containerItemList">
+
+                </nav>
+
+                <div className="containerCart">
+
+                </div>
+
+            </header>
+    )
+}
+
+export default NavBar;
